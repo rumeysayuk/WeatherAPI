b91ba65761629ba81688f3593919d09a84e648d0
Rümeysa yük <19110131041@ogr.ksu.edu.tr>
2021-08-23 10:26:03 +0300

    .

--- js/app.js
+++ js/app.js
@@ -1,60 +1,18 @@
-// function getWeather() {
-//     const location = document.getElementById("location");
-//     const weather = document.getElementById("weather");
-//     let api = "https://api.openweathermap.org/data/2.5/weather";
-//     let apiKey = "5e298e728c465424ef66f0b04798fcab";
-//     let api2 = "https://api.openweathermap.org/data/2.5/weather";
-//
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//
-//     function resolve(position) {
-//         latitude = position.coords.latitude;    //enlem
-//         longitude = position.coords.longitude;   //boylam
-//
-//         let url = api + "?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=metric";
-//
-//
-//         fetch(url)
-//             .then(res => res.json())
-//             .then(data => {
-//                 console.log(data);
-//                 let temp = data.main.feels_like;
-//                 weather.innerHTML = `
-//                    <div class="card" style="width: 22rem;">
-//                    <img class="card-img-top" src="weather.gif" alt="Weather">
-//                         <div class="card-body">
-//     <h5 class="card-title ">${temp} ° C</h5>
-//     <p class="card-text">${data.name}</p>
-//     <p class="card-text text">${data.weather[0].main}</p>
-//     <a href="#" class="btn btn-primary" onclick="window.location.reload(false)">Hava Durumune yenile</a>
-//   </div>
-// </div>
-//        `
-//             });
-//     }
-//
-//     function reject() {
-//         location.innerHTML = "Konumunuz alınamadı";
-//     }
-// }
-//getWeather();
-function getWeather(cityName) {
-    const show = document.querySelector("#loc")
+async function getWeather(cityName) {
     let apiKey = "5e298e728c465424ef66f0b04798fcab";
-    let api2 = "https://api.openweathermap.org/data/2.5/weather";
-    let url = api2 + "?q=" + cityName + "&appid=" + apiKey + "&units=metric&lang=tr";
-
-    fetch(url)
+    let api = "https://api.openweathermap.org/data/2.5/weather";
+    let url = api + "?q=" + cityName + "&appid=" + apiKey + "&units=metric&lang=tr";
+    let weather;
+    await fetch(url)
         .then(res => res.json())
         .then(data => {
-           //console.log(data)
-            let temp = data.main.temp;
-            let name =data.name;
-           // console.log(temp + " °C")
-            show.innerHTML = `${name} : ${temp} °C`
-            return temp;
-        })
 
+             let temp = data.main.temp;
+            console.log(temp)
+            //let temp2=data.list[1].main.temp;
+            weather = temp;
+        })
+    return weather;
 }
 
 /* SVG Türkiye haritasi js */
@@ -64,16 +22,19 @@ function svgturkiyeharitasi() {
     const info = document.querySelector('.il-isimleri');
 
     element.addEventListener(
-        'mouseover',
+        'click',
         function (event) {
             if (event.target.tagName === 'path' && event.target.parentNode.id !== 'guney-kibris') {
                 info.innerHTML = [
-                    '<div>',
+                    '<div id="temp" class="temp">',
                     event.target.parentNode.getAttribute('data-iladi'),
                     '</div>'
                 ].join('');
                 const cityName = event.target.parentNode.getAttribute('data-iladi');
-                getWeather(cityName)
+                 getWeather(cityName).then((temp)=>{
+                     const show=document.querySelector("#temp")
+                     show.innerHTML+=" "+temp+" °C";
+                 })
             }
         }
     );
